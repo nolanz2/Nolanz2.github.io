18ea8d0845f26def55e37043f60d3b9e08bfae75
nolanz2 <ncligro1@jhu.edu>
2019-04-02 08:48:47 -0400

    New bgs

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,8 @@
+//Load random image from .../img/bg for background image
 var bgs = [
-        'img/bg/bg.jpg', 'img/bg/bg2.jpg', 'img/bg/bg3.jpg' , 'img/bg/bg4.jpg'
+        'img/bg/bg.jpg', 'img/bg/bg2.jpg', 'img/bg/bg3.jpg' , 
+		'img/bg/bg4.jpg' , 'img/bg/bg5.jpg'
+		//'https://source.unsplash.com/collection/1368747/1600x900'
     ];
 
     window.onload = function () {
