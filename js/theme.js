5d63f7bb4423eaa6c94a7ddcfb76a5d48a7adfd5
nolanz2 <ncligro1@jhu.edu>
2019-04-05 23:08:40 -0400

    Change favicon and background images

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,8 @@
 //Load random image from .../img/bg for background image
 var bgs = [
         'img/bg/bg.jpg', 'img/bg/bg2.jpg', 'img/bg/bg3.jpg' , 
-		'img/bg/bg4.jpg' , 'img/bg/bg5.jpg' , 'img/bg/bg6.jpg'
+		'img/bg/bg4.jpg' , 'img/bg/bg5.jpg' , 'img/bg/bg6.jpg' ,
+		'img/bg/bg7.jpg' , 'img/bg/bg8.jpg' , 'img/bg/bg9.jpg' , 'img/bg/bg10.jpg'
 		//'https://source.unsplash.com/collection/1368747/1600x900'
     ];
 
